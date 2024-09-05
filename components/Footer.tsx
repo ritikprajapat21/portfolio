b2d1812d07c69c87944ce20b76611a56d62f2c82
ritikprajapat21 <ritikprajapati084@gmail.com>
2024-09-05 18:11:39 +0530

    Fixed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,10 @@ import Links from "./Links";
 const Footer = () => {
   return (
     <div className="flex justify-between items-center bg-purple-400/20 backdrop-blur-md p-2 md:px-6">
-      <p>Designed by Ritik Prajapat</p>
-      <p>Copyright &copy; {new Date().getFullYear()} RP</p>
+      <div className="leading-loose text-balance">
+        <p>Designed by Ritik Prajapat</p>
+        <p>Copyright &copy; {new Date().getFullYear()} RP</p>
+      </div>
       <Links />
     </div>
   );
